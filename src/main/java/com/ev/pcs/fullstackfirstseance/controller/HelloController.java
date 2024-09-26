396ecd91c3faaab009bb62dc7123aaf0f531dd14
Imane <imaneerraji17@gmail.com>
2024-09-27 00:09:45 +0100

    update main

--- src/main/java/com/ev/pcs/fullstackfirstseance/controller/HelloController.java
+++ src/main/java/com/ev/pcs/fullstackfirstseance/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String BienvenueSpringCode(){
-        return "Bienvenue Sp Code";
+        return "Bienvenue Spri Code";
     }
 }
